c1b9a8b516d167cf3bcae3d1bbc99e38440ece3f
Victor Sousa <victor.sousa@sosoxygene.com>
2023-10-12 18:20:20 +0200

    removed tailwind test button

--- apps/site/src/App.tsx
+++ apps/site/src/App.tsx
@@ -13,7 +13,6 @@ function App() {
           Learn React
         </a>
       </header>
-      <button className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">fdsfdsd</button>
     </div>
   );
 }
