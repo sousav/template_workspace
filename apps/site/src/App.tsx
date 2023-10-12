36d85ce9706926ba1d8d4573d533683e44c7637c
Victor Sousa <victor.sousa@sosoxygene.com>
2023-10-12 18:19:19 +0200

    Init monorepo workspace

---
+++ apps/site/src/App.tsx
@@ -0,0 +1,21 @@
+import './App.css';
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+      </header>
+      <button className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">fdsfdsd</button>
+    </div>
+  );
+}
+
+export default App;
